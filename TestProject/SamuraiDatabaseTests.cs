11842e9c6a34f3f634c3953f521474cb79f1d55f
markopedu <69790644+markopedu@users.noreply.github.com>
2020-11-30 10:04:17 +0100

    Delete Samurai

--- TestProject/SamuraiDatabaseTests.cs
+++ TestProject/SamuraiDatabaseTests.cs
@@ -10,18 +10,52 @@ namespace TestProject
     public class SamuraiDatabaseTests
     {
 
-        [Fact]
-        public void CanInsertSamuraiIntoDatabase()
+        private void CreateDb(Action<SamuraiContext> action)
         {
             var builder = new DbContextOptionsBuilder<SamuraiContext>();
             builder.UseInMemoryDatabase("InsertSamurai");
 
             using var context = new SamuraiContext(builder.Options);
-            
-            var samurai = new Samurai();
-            context.Samurais.Add(samurai);
-            context.SaveChanges();
-            Assert.NotEqual(0, samurai.Id);
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+            action(context);
+        }
+        
+        [Fact]
+        public void CanInsertSamuraiIntoDatabase()
+        {
+            Action<SamuraiContext> action = (context) =>
+            {
+                var samurai = new Samurai();
+                context.Samurais.Add(samurai);
+                context.SaveChanges();
+                Assert.NotEqual(0, samurai.Id);
+            };
+           
+            CreateDb(action);
+           
+        }
+        
+        [Fact]
+        public void CanDeleteSamuraiIntoDatabase()
+        {
+            Action<SamuraiContext> action = (context) =>
+            {
+                var samurai = new Samurai();
+                context.Samurais.Add(samurai);
+                context.SaveChanges();
+                Assert.NotEqual(0, samurai.Id);
+
+                var id = samurai.Id;
+                context.Samurais.Remove(samurai);
+                context.SaveChanges();
+
+                var deletedSamurai = context.Samurais.Find(id);
+                Assert.Null(deletedSamurai);
+            };
+           
+            CreateDb(action);
+           
         }
     }
 }
